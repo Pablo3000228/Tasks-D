d66b91cb62892ad12f8da0061fd78ca33017928b
Largee <igrok3138969@gmail.com>
2026-09-25 17:53:03 +0500

    Task #4

---
+++ find_simple_number.py
@@ -0,0 +1,6 @@
+n = int(input("Введите число: "))
+
+nums = [x for x in range(2, n)]
+
+for i in nums:
+    
--- text_analyze.py
+++ text_analyze.py
@@ -1,9 +1,9 @@
 text = input("Введите текст: ").lower()
-max = 0
 freq_symbols = {}
 for el in text:
     count = text.count(el)
     freq_symbols[el] = count
 s = sorted(freq_symbols, key=lambda x: freq_symbols[x], reverse=True)
+for letter in freq_symbols:
+    print(f"Кол-во элемента {letter}: {freq_symbols[letter]}")
 print(s[:3])
-    
